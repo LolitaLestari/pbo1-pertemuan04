e8417184ccb0f5502235d75fd68a8c61f8fe2fad
LolitaLestari <lolita9a@gmail.com>
2020-04-27 12:29:58 +0800

    tugas perulangan

--- src/Tugas.java
+++ src/Tugas.java
@@ -1,2 +1,11 @@
 public class Tugas {
+    public static void main(String[] args){
+        int i = 0;
+        int j = 20;
+
+        for (i = 0 ; i<= j ; i++){
+            System.out.println("Berhitung : " + i + "Kg");
+        }
+        System.out.println("Finale : Berhitung sampai  : " + i + "Kg");
+    }
 }
