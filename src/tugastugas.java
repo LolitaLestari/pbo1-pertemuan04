cdc8a00617c7d95661836f4a7470d1af7b8810ca
LolitaLestari <lolita9a@gmail.com>
2020-04-27 12:40:44 +0800

    tugas perulangan ARRAY

--- src/tugastugas.java
+++ src/tugastugas.java
@@ -1,2 +1,11 @@
 public class tugastugas {
+    public static void main(String[] args) {
+        String[][] Nama = { {"Lolita","Teknik Informatika"},{"Ayu","EKonomi"},{"Peby","Ekonomi"}};
+        for(int x=0; x< Nama.length; x++){
+            System.out.println("Nama : "+ Nama[x][0]);
+            System.out.println("Jurusan : "+ Nama[x][1]);
+            System.out.println("--------------------------");
+
+        }
+    }
 }
